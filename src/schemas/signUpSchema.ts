0ae698463e85a5bd6d24f8e4008e2d2f450b7dbe
Princek93 <princed55x@gmail.com>
2025-10-05 01:25:54 +0530

    password update

--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -10,5 +10,5 @@ export const usernameValidation = z
 export const signUpSchema = z.object({
     username : usernameValidation,
     email: z.string().regex(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, { message: "Invalid email address" }),
-    
+    password: z.string().min(6, {message: "password must be at least 6 characters"}),
 })
