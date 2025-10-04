886684453f6db1e1daa31494140d58eba516dd7f
Princek93 <princed55x@gmail.com>
2025-10-05 00:46:56 +0530

    spacex

---
+++ src/schemas/signUpSchema.ts
@@ -0,0 +1,14 @@
+import {z} from 'zod'
+
+export const usernameValidation = z
+              .string()
+              .min(2, {message: "Username must be at least 2 characters"})
+              .max(20, {message: "Username must be at most 20 characters"})
+              .regex(/^[a-zA-Z0-9_]+$/, { message: "Username can only contain letters, numbers, and underscores" })
+
+
+export const signUpSchema = z.object({
+    username : usernameValidation,
+    email: z.string().regex(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, { message: "Invalid email address" }),
+    
+})
