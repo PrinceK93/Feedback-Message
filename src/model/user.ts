95b6df9ff65b8bf871e9f8a222c19b5e98104ba6
Princek93 <princed55x@gmail.com>
2025-10-04 19:36:37 +0530

    user upadte

--- src/model/user.ts
+++ src/model/user.ts
@@ -1,5 +1,18 @@
 import mongoose, {Schema , Document} from "mongoose";
 
-export interface Message {
+export interface Message extends Document{
     content: string;
+    createdAt: Date;
 }
+
+const messageschema: Schema<Message> = new Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+})
